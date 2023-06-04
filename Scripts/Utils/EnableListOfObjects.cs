10ee58283aaab8d46318707ab2f2f8958a9f018d
lukeskt <hello@lukeskarth.com>
2023-06-05 00:11:16 +0100

    add old remes utils
    might dispose some yet

---
+++ Scripts/Utils/EnableListOfObjects.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AttentionDrivenScenography
+{
+    public class EnableListOfObjects : MonoBehaviour
+    {
+        public GameObject[] objectsToActivate;
+
+        // Start is called before the first frame update
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                // Activate all specified objects (i.e. empty containers for reactive elements)
+                foreach (var obj in objectsToActivate)
+                {
+                    obj.SetActive(true);
+                }
+            }
+            // Remove this component - but keep trigger in case useful later?
+            Destroy(this);
+        }
+    }
+}
