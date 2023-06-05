f178104b467d19ff5cd5020a199fce663ba06c37
lukeskt <hello@lukeskarth.com>
2023-06-05 14:40:53 +0100

    editor stubs probably can delete later

---
+++ Scripts/Editor/AttentionBehaviourEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace AttentionDrivenScenography
+{
+    [CustomEditor(typeof(AttentionBehaviour))]
+    [CanEditMultipleObjects]
+    public class AttentionBehaviourEditor : Editor
+    {
+
+    }
+}
---
+++ Scripts/Editor/ComparisonAttentionBehaviourEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace AttentionDrivenScenography
+{
+    public class ComparisonAttentionBehaviourEditor : Editor
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
