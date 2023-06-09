9c997fcd08b45168e151f4f4a6f67fb87982a3bd
lukeskt <hello@lukeskarth.com>
2023-06-09 22:06:17 +0100

    add stubs for example interactions

---
+++ Scripts/AttentionBehaviours/AudioSource/CurrentAttentionComparisonBehaviourAudioSource.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace AttentionDrivenScenography
+{
+    public class CurrentAttentionComparisonBehaviourAudioSource : AttentionComparisonBehaviour
+    {
+        public AudioSource audioSource;
+                               
+        public override void AttentionEffect()
+        {
+
+        }
+    }
+}
